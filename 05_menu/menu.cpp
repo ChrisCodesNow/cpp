2aeb2616db56929c890e1257b046510704015961
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 10:50:41 -0600

    modularize display menu

--- 05_menu/menu.cpp
+++ 05_menu/menu.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// Function prototypes
+void display_menu();
 
 int main(){
     int choice;
     do{
-        cout << "Main Menu" << endl;
-        cout << "Book Rental System" << endl;
-        cout << "---" << endl;
-
-        cout << "Select an option" << endl;
-        cout << "1. Book Management" << endl;
-        cout << "2. Member Management" << endl;
+        display_menu();
     }while(false);
+}
+
+void display_menu(){
+    cout << "Main Menu" << endl;
+    cout << "Book Rental System" << endl;
+    cout << "---" << endl;
+
+    cout << "Select an option" << endl;
+    cout << "1. Book Management" << endl;
+    cout << "2. Member Management" << endl;
 }
