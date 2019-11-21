60be14ae0c75862c56ed8d4193ee1f507ca1dd02
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 10:45:23 -0600

    add menu skeleton

---
+++ 05_menu/menu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int choice;
+    do{
+        cout << "Main Menu" << endl;
+        cout << "Book Rental System" << endl;
+        cout << "---" << endl;
+
+        cout << "Select an option" << endl;
+        cout << "1. Book Management" << endl;
+        cout << "2. Member Management" << endl;
+    }while(false);
+}
