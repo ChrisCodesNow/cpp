7f858a1ae078c0c29e9dcda51a73531e0f4e62fa
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-20 18:36:34 -0600

    get cypher key

--- 04_arrays/caesar.cpp
+++ 04_arrays/caesar.cpp
@@ -4,10 +4,11 @@ using namespace std;
 // Function prototype
 void display_message();
 int get_key();
-int get_choice();
+
 
 int main(){
     int key;
+    int choice;
     do{
         display_message();
         key = get_key();
@@ -24,4 +25,16 @@ void display_message(){
     cout << "2. Decrypt a message" << endl;
     cout << "3. Change the key" << endl;
     cout << "4. Exit"  << endl;
+}
+
+int get_key(){
+    int key = 0;
+    cout << "Select a key: ";
+    cin >> key;
+
+    if(key == 0){
+        cout << "Using a key of 0 has no effect.\n";
+        cout << "Please select a different key before attempting to encrypt or decrypt\n";
+    }
+    return key;
 }
