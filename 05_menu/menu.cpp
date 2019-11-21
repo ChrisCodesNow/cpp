e2b4f82e35f3bb66b14fd41555caabb722cabd34
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 15:33:05 -0600

    add select book choice skeleton

--- 05_menu/menu.cpp
+++ 05_menu/menu.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void display_menu();
 void select_choice(int choice);
 void display_book_menu();
+void select_book_choice(int);
 
 int main(){
     int choice;
@@ -32,7 +33,7 @@ void select_choice(int choice){
         display_book_menu();
         int book_choice;
         cin >> book_choice;
-        
+        select_book_choice(book_choice);
         break;
     case 2:
         cout << "Welcome to member management" << endl;
@@ -50,4 +51,33 @@ void display_book_menu(){
     cout << "6. Remove x from stock" << endl;
     cout << "7. Sell/Salvage book (stock --)" << endl;
     cout << "8. Update book information" << endl;
+}
+
+void select_book_choice(int book_choice){
+    switch(book_choice){
+    case 1: 
+        cout << "Check out selected" << endl;
+        break;
+    case 2:
+        cout << "Return selected" << endl;
+        break;
+    case 3:
+        cout << "Add book selected" << endl;
+        break;
+    case 4:
+        cout << "Remove book selected" << endl;
+        break;
+    case 5:
+        cout << "Add to stock selected" << endl;
+        break;
+    case 6:
+        cout << "Remove from stock selected" << endl;
+        break;
+    case 7:
+        cout << "Sell/Salvage selected" << endl;
+        break;
+    case 8:
+        cout << "Update book info selected" << endl;
+        break;
+    }
 }
