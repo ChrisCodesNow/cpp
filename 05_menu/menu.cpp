7dbe2d599a64dc1de8e02912e07ca9e25831fbe1
c <30329315+ChrisCodesNow@users.noreply.github.com>
2019-11-21 10:59:04 -0600

    modularize select choice

--- 05_menu/menu.cpp
+++ 05_menu/menu.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 // Function prototypes
 void display_menu();
+void select_choice(int choice);
 
 int main(){
     int choice;
     do{
         display_menu();
+        cin >> choice;
+        select_choice(choice);
     }while(false);
 }
 
@@ -19,4 +22,15 @@ void display_menu(){
     cout << "Select an option" << endl;
     cout << "1. Book Management" << endl;
     cout << "2. Member Management" << endl;
+}
+
+void select_choice(int choice){
+    switch(choice){
+    case 1:
+        cout << "Welcome to book management" << endl;
+        break;
+    case 2:
+        cout << "Welcome to member management" << endl;
+        break;
+    }
 }
